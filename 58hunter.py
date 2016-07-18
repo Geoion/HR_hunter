64a447d37354d0ffd6f03f7e725f0ee2b0860256
蓝破碎 <Zeninexu@gmail.com>
2016-07-18 22:46:59 +0800

    #第二版，邮箱补丁

--- 58hunter.py
+++ 58hunter.py
@@ -11,6 +11,14 @@ from bs4 import BeautifulSoup
 import time
 #coding changing
 reload(sys)
+#SQL_RESULT
+#1.SELECT * FROM company58 WHERE email !='' AND LENGTH(PHONE_NO)==11 AND SUBSTR(PHONE_NO,1,1)=='1'
+#2.SELECT * FROM company58 WHERE email =='' AND LENGTH(PHONE_NO)==11 AND SUBSTR(PHONE_NO,1,1)=='1'
+#3.SELECT * FROM company58 WHERE  not (LENGTH(PHONE_NO)==11 AND SUBSTR(PHONE_NO,1,1)=='1') and hr!=""
+
+
+
+
 #print sys.getdefaultencoding()
 sys.setdefaultencoding("utf-8")
 headers = {'User-Agent': '"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0"'}
@@ -19,8 +27,8 @@ opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
 
 
 #数据库名称及城市名称进行设置
-city="hz"
-conn = sqlite3.connect("HR_hunter_sh.db")
+city = "hz"
+conn = sqlite3.connect("HR_hunter_hz.db")
 #设置区
 
 business_list={'zplvyoujiudian':'餐饮','jiazhengbaojiexin':'家政保洁/安保','meirongjianshen':'美容/美发','zpjiudian':'酒店/旅游','zpwentiyingshi':'娱乐/休闲','zpanmo':'保健按摩','zpjianshen':'运动健身','renli':'人事/行政/后勤','siji':'司机','zpguanli':'高级管理','yewu':'销售','kefu':'客服','zpshangwumaoyi':'贸易/采购','chaoshishangye':'超市/百货/零售','zptaobao':'淘宝职位','zpfangchan':'房产中介','shichang':'市场/媒介/公关','zpguanggao':'广告/会展/咨询','zpmeishu':'美术/设计/创意','zpshengchankaifa':'普工/技工','zpshengchan':'生产管理/研发','zpwuliucangchu':'物流/仓储','xiaofeipin':'服装/纺织/食品','zhikonganfang':'质控/安防','zpqiche':'汽车制造/服务','tech':'计算机/互联网/通信','zpjixieyiqi':'电子/电气','zpjixie':'机械/仪器仪表','zpfalvzixun':'法律','zhuanye':'教育培训','fanyizhaopin':'翻译','zpxiezuochuban':'编辑/出版/印刷','zpcaiwushenji':'财务/审计/统计','jinrongtouzi':'金融/银行/证券/投资','zpjinrongbaoxian':'保险','zpyiyuanyiliao':'医院/医疗/护理','zpzhiyao':'制药/生物工程','huanbao':'环保','zpfangchanjianzhu':'建筑','zpwuye':'物业管理','nonglinmuyu':'农/林/牧/渔业','zhaopin':'其他职位'}
@@ -69,6 +77,8 @@ def get_info(id):
         return ["","",""]
     try:
         email=htmlSource.xpath("/html/body/div[1]/div[5]/div/div[2]/dl/dd[2]")[0].text
+        if email == "\r\n\t\t\t\t\t\t" :
+            email = ""
     except Exception as e :
         email=""
 
@@ -111,7 +121,18 @@ def patch():
         time.sleep(2)
     print "updated:" +str(count) +" all :" +str(length)
     #print result
+def patch2():
 
+    sql_inq = "select * from company58 where email like '%	%' "
+    cu = conn.cursor()
+    cu.execute(sql_inq)
+    result = cu.fetchall()
+    length = len(result)
+    for i in range(length):
+        sql_update = "update company58 set EMAIL= '' WHERE COMPANY_ID= '" + result[i][6] + "'"
+        cu.execute(sql_update)
+        conn.commit()
+    print str(length)+"EMAIL have been updated"
 
 
 
@@ -123,4 +144,5 @@ for business in business_list:
         except Exception as e:
             print e,city,business,page,
 patch()
+patch2()
 conn.close()
